0b19c67a29fa8dbf359b96b478d1d666fc0d5387
DialloAbdou <abdoudiallo3826@yahoo.fr>
2025-03-05 16:30:11 +0100

    ajout de redis pour  cache partage

---
+++ minimalApiPro/AspWebApi/DistributedExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace AspWebApi
+{
+    public static class DistributedExtensions
+    {
+        public static async Task<T?>GetAsync<T>( this IDistributedCache cache, string key)
+            where T : class
+        {
+            var json = await cache.GetStringAsync(key);
+            if (string.IsNullOrEmpty(json)) return default;
+            return JsonSerializer.Deserialize<T>(json, new JsonSerializerOptions
+            {
+                 PropertyNameCaseInsensitive = true,
+            });
+           
+          
+       
+        }
+
+        public static  async Task SetAsync<T>(this IDistributedCache cache, String key, T value)
+            where T : class
+        {
+               var json =  JsonSerializer.Serialize(cache);
+              await cache.SetStringAsync(key, json);
+        }
+
+    }
+}
--- minimalApiPro/AspWebApi/Program.cs
+++ minimalApiPro/AspWebApi/Program.cs
@@ -5,8 +5,10 @@ using AspWebApi.Data.Models;
 using AspWebApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Caching.Distributed;
+using AspWebApi;
 
-var builder = WebApplication.CreateBuilder();
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.ClearProviders();
 
@@ -23,7 +25,19 @@ builder.Services.AddValidatorsFromAssemblyContaining<Program>(); // fluentvalida
 builder.Services.AddDbContext<ApiDbContext>(opt => opt.UseSqlite(
     builder.Configuration.GetConnectionString("sqlite")));
 
-builder.Services.AddMemoryCache();
+//builder.Services.AddMemoryCache();
+//builder.Services.AddOutputCache(opt =>
+//{
+//    opt.AddBasePolicy(b => b.Expire(TimeSpan.FromMinutes(1)));
+//    opt.AddPolicy("Expire2min", b=>b.Expire(TimeSpan.FromMinutes(2)));
+//    opt.AddPolicy("Expire10sec", b=>b.Expire(TimeSpan.FromSeconds(10)));
+//    opt.AddPolicy("ById", b => b.SetVaryByRouteValue("id"));
+//});
+//builder.Services.AddDistributedMemoryCache();
+builder.Services.AddStackExchangeRedisCache(opt =>
+{
+    opt.Configuration = "localhost:6379";
+});
 
 var app = builder.Build();
 
@@ -31,6 +45,7 @@ app.Services.
     CreateScope().
     ServiceProvider.
     GetRequiredService<ApiDbContext>().Database.EnsureCreated();
+//app.UseOutputCache();
 #region hello
 //app.MapGet("/hello/{nom}",(
 //   [FromRoute] String nom,
@@ -68,27 +83,40 @@ app.MapGet("/personne", async (
 app.MapGet("/personne/{id:int}", async (
     [FromRoute] int id,
     [FromServices] ApiDbContext context,
-    [FromServices] IMemoryCache cache) =>
+    [FromServices] IDistributedCache cache
+    /*[FromServices] IMemoryCache cache*/) =>
 {
-    if (!cache.TryGetValue<Personne>($"personne_{id}", out var personne))
+    var personne = await cache.GetAsync<Personne>($"personne_{id}");
+    if (personne is null)
     {
-        personne = await context.Personnes.FirstOrDefaultAsync(p => p.Id == id);
-        if (personne is not null)
-        {
-            cache.Set($"personne{id}", personne);
-            return Results.Ok(personne);
-        }
-        return Results.NotFound();
+        personne = await context.Personnes.Where(p => p.Id == id).FirstOrDefaultAsync();
+        if (personne is null) return Results.NotFound();
+        await cache.SetAsync($"personne_{id}", personne);
+        return Results.Ok(personne);
     }
     return Results.Ok(personne);
-    
+    //if (!cache.GetAsync<Personne>($"personne_{id}", out var personne))
+    //{
+    //    personne = await context.Personnes.FirstOrDefaultAsync(p => p.Id == id);
+    //    if (personne is not null)
+    //    {
+    //        cache.SetAsync($"personne{id}", personne);
+    //        return Results.Ok(personne);
+    //    }
+    //    return Results.NotFound();
+    //}
+    //var personne = await context.Personnes.FirstOrDefaultAsync(x => x.Id == id);
+    //if (personne is not null) return Results.Ok(personne);
+    //return Results.NotFound();
+
 });
 
 app.MapPut("/personne/{id:int}", async (
     [FromRoute] int id,
     [FromBody] Personne personne,
     [FromServices] ApiDbContext context,
-    [FromServices] IMemoryCache cache) =>
+      [FromServices] IDistributedCache cache
+   /*[FromServices] IMemoryCache cache*/) =>
 {
     var resulta = await context.Personnes
    .Where(p => p.Id == id)
@@ -96,8 +124,8 @@ app.MapPut("/personne/{id:int}", async (
                                             .SetProperty(p => p.Prenom, personne.Prenom));
     if (resulta > 0)
     {
-        cache.Remove($"personne_{id}");
-         return Results.NoContent();
+        await cache.RemoveAsync($"personne_{id}");
+        return Results.NoContent();
     }
     return Results.NotFound();
 
