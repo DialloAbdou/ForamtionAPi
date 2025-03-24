b09555f295a475bdf1d0e45c524cbf6eadfa07d2
DialloAbdou <abdoudiallo3826@yahoo.fr>
2025-03-24 22:13:11 +0100

    ajout documentations

--- TpTodo/TodoList/Endpoints/TaskEndpoints.cs
+++ TpTodo/TodoList/Endpoints/TaskEndpoints.cs
@@ -1,15 +1,145 @@
-﻿using System.Runtime.CompilerServices;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using TodoList.Dto;
+using TodoList.Services;
 
 namespace TodoList.Endpoints
 {
-    public  static class TaskEndpoints
+    public static class TaskEndpoints
     {
         public static RouteGroupBuilder GetTaskEndpoints(this RouteGroupBuilder groupe)
         {
-            //groupe.MapGet("/", (TaskServices taskServices) => taskServices.GetAll());
+            groupe.MapGet("", GetAllTaskAsync)
+                .Produces<TaskOutPutModel>(statusCode: 200, contentType: "applicaton/json")
+                .WithTags("Gestionnaire des Taches");
+
+            groupe.MapGet("/{id:int}", GetTaskByIdAsync)
+                .Produces<TaskOutPutModel>(statusCode: 200, contentType: "applicaton/json")
+                .WithTags("Gestionnaire des Taches");
+
+            groupe.MapGet("/active", GetAllTaskActivesAsync)
+                .Produces<TaskOutPutModel>(statusCode: 200, contentType: "applicaton/json")
+                .WithTags("Gestion des Taches");
+
+            groupe.MapPost("", AddTaskAsync)
+                .Produces<TaskOutPutModel>(statusCode:200,contentType:"applicaton/json")
+                .WithTags("Gestionnaire des Taches");
+
+            groupe.MapPut("/{id:int}", UpdateTaskAsync)
+                .Produces(204)
+                .Produces(404)
+                .WithTags("Gestionnaire des Taches");
             return groupe;
         }
 
-     
+        /// <summary>
+        /// Elle renvoie la liste des taches
+        /// </summary>
+        /// <param name="taskServices"></param>
+        /// <returns></returns>
+        private static async Task<IResult> GetAllTaskAsync
+            (
+                 [FromServices] ITaskService taskServices
+            )
+        {
+            var taks = await taskServices.GetAllTasks();
+            if (taks is null) return Results.NoContent();
+            return Results.Ok(taks);
+        }
+
+        /// <summary>
+        /// elle renvoie un element de taches
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="taskServices"></param>
+        /// <param name="logger"></param>
+        /// <returns></returns>
+        private static async Task<IResult> GetTaskByIdAsync
+            (
+              [FromRoute] int id,
+              [FromServices] ITaskService taskServices,
+              [FromServices] ILogger<Program> logger
+            )
+        {
+            var task = await taskServices.GetTaskById(id);
+            logger.LogInformation("le nom de l'identifiant est {id},{title} ", id, task.Title);
+            if (task is not null) return Results.Ok(task);
+            return Results.NotFound();
+        }
+
+        /// <summary>
+        /// liste des Actives
+        /// </summary>
+        /// <param name="taskServices"></param>
+        /// <param name="logger"></param>
+        /// <returns></returns>
+        private static async Task<IResult> GetAllTaskActivesAsync
+            (
+                [FromServices] ITaskService taskServices,
+                [FromServices] ILogger<Program> logger
+            )
+        {
+            var todoactives = await taskServices.GetTaskActive();
+            if (todoactives is null) return Results.NoContent();
+            return Results.Ok(todoactives);
+        }
+
+        /// <summary>
+        /// Ajout de taches 
+        /// </summary>
+        /// <param name="taskImput"></param>
+        /// <param name="validator"></param>
+        /// <param name="taskServices"></param>
+        /// <param name="logger"></param>
+        /// <returns></returns>
+
+        private static async Task<IResult> AddTaskAsync
+            (
+                [FromBody] TaskInputModel taskImput,
+                [FromServices] IValidator<TaskInputModel> validator,
+                [FromServices] ITaskService taskServices,
+                [FromServices] ILogger<Program> logger
+            )
+        {
+            var result = validator.Validate(taskImput);
+            if (!result.IsValid) return Results.BadRequest(result.Errors.Select(e => new
+            {
+                e.ErrorMessage,
+                e.PropertyName
+
+            }));
+            var mytak = await taskServices.AddTask(taskImput);
+            return Results.Ok(mytak);
+        }
+
+        /// <summary>
+        /// /Mise a jour
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="task"></param>
+        /// <param name="taskServices"></param>
+        /// <param name="logger"></param>
+        /// <returns></returns>
+        private static async Task<IResult> UpdateTaskAsync
+            (
+              [FromRoute] int id,
+              [FromBody] TaskInputModel task,
+              [FromServices] ITaskService taskServices,
+              [FromServices] ILogger<Program> logger)
+        {
+            var result = await taskServices.UpdateTask(id, task);
+            if (result)
+            {
+                return Results.Ok($"it's {result} ");
+                logger.LogInformation("voici indentifiant modifier {id}", id);
+            }
+            return Results.NotFound();
+
+        }
+
+
+
+
+
     }
 }
--- TpTodo/TodoList/Program.cs
+++ TpTodo/TodoList/Program.cs
@@ -8,10 +8,14 @@ using TodoList.Data;
 using TodoList.Dto;
 using TodoList.Endpoints;
 using TodoList.Services;
+
 var builder = WebApplication.CreateBuilder();
+
 builder.Logging.ClearProviders();
+
 var loggerConfiguration = new LoggerConfiguration()
     .WriteTo.Console();
+
 var logger = loggerConfiguration.CreateLogger();
 builder.Logging.AddSerilog(logger);
 
@@ -19,74 +23,31 @@ builder.Services.AddDbContext<TaskDbContext>(
     op => op.UseSqlite(builder.Configuration.GetConnectionString("sqlite")));
 
 builder.Services.AddScoped<ITaskService, TaskServices>();
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-var app = builder.Build();
 
+builder.Services.AddEndpointsApiExplorer(); // permet d'explorer les differents endointd dont on dispose
+builder.Services.AddSwaggerGen(); // support de swagger
+
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+var app = builder.Build();
+#region Migration
 //app.Services
 //    .CreateScope().ServiceProvider
 //    .GetRequiredService<TaskDbContext>().Database
 //    .EnsureCreated(); // creer db sans utiliser les lignes de commandes
 
- //await app.Services
- //   .CreateScope().ServiceProvider
- //   .GetRequiredService<TaskDbContext>().Database
- //   .MigrateAsync(); // mise a jour de DB
+//await app.Services
+//   .CreateScope().ServiceProvider
+//   .GetRequiredService<TaskDbContext>().Database
+//   .MigrateAsync(); // mise a jour de DB
+#endregion!
 
-
-//app.MapGroup("/todos").GetTaskEndpoints();
-
-app.MapGet("/todos",  async(
-    [FromServices] ITaskService taskServices) =>  await taskServices.GetAllTasks());
-
-app.MapGet("/todos/{id:int}",  async(
-    int id, 
-    [FromServices] ITaskService taskServices,
-    [FromServices] ILogger<Program> logger) =>
+ app.MapGroup("/todos").GetTaskEndpoints();
+if(app.Environment.IsDevelopment())
 {
-    var task = await  taskServices.GetTaskById(id);
+    app.UseSwagger();// permet de generer le fichier swagger
+    app.UseSwaggerUI(); //permet de generer l'interface graphique 
 
-    logger.LogInformation("le nom de l'identifiant est {id},{title} ", id, task.Title);
-    if (task is not null) return Results.Ok(task);
-    return Results.NotFound();
-});
-
-app.MapGet("/todos/active", async (
-       [FromServices] ITaskService taskServices,
-       ILogger<Program> logger) =>
-{
-    var todoactives = await taskServices.GetTaskActive();
-
-    return Results.Ok(todoactives);
-});
-
-app.MapPost("/todos",  async (
-                       [FromBody] TaskInputModel taskImput,
-                      [FromServices] IValidator<TaskInputModel> validator,
-                      [FromServices] ITaskService taskServices,
-                      [FromServices] ILogger<Program> logger
-                ) =>
-{
-    var result = validator.Validate(taskImput);
-    if (!result.IsValid) return Results.BadRequest(result.Errors.Select(e => new
-    {
-        e.ErrorMessage,
-        e.PropertyName
-
-    }));
-    var mytak =   await taskServices.AddTask(taskImput);
-    return Results.Ok(mytak);
-
-});
-
-app.MapPut("/todos/{id:int}",async (
-      [FromRoute] int id,
-      [FromBody] TaskInputModel task,
-      [FromServices] ITaskService taskServices,
-       [FromServices] ILogger<Program> logger) =>
-{
-    var taskodl =  await taskServices.UpdateTask(id, task);
-    logger.LogInformation("voici indentifiant modifier {id}", id);
-    return Results.Ok(taskodl);
-});
+}
 
 app.Run();
